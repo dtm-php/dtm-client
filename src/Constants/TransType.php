7aa41ce606d851fab596b050fe8ddfb9175c0a78
huangzhhui <huangzhwork@gmail.com>
2022-02-08 01:42:29 +0800

    Create TransType.php

---
+++ src/Constants/TransType.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of DTM-PHP.
+ *
+ * @license  https://github.com/dtm-php/dtm-client/blob/master/LICENSE
+ */
+namespace DtmClient\Constants;
+
+class TransType
+{
+    public const TCC = 'tcc';
+
+    public const SAGE = 'sage';
+
+    public const XA = 'xa';
+
+    public const MSG = 'msg';
+}
