015ff43d223e1a46b9b542f56c32b3ff00f93ee7
huangzhhui <huangzhwork@gmail.com>
2022-02-08 01:27:07 +0800

    Create Sage.php

---
+++ src/Sage.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of DTM-PHP.
+ *
+ * @license  https://github.com/dtm-php/dtm-client/blob/master/LICENSE
+ */
+
+namespace DtmClient;
+
+use DtmClient\Api\ApiInterface;
+
+class Sage
+{
+
+    protected array $orders = [];
+
+    protected bool $concurrent = false;
+
+    protected ApiInterface $api;
+
+    public function __construct(ApiInterface $api)
+    {
+        $this->api = $api;
+    }
+
+    public function init(string $gid)
+    {
+        TransContext::init($gid, 'sage', '');
+    }
+
+    public function add(string $action, string $compensate, $payload): static
+    {
+        TransContext::appendSteps([
+            'action' => $action,
+            'compensate' => $compensate,
+        ]);
+        TransContext::appendPayloads(json_encode($payload));
+        return $this;
+    }
+
+    public function addBranchOrder(int $branch, array $preBranches): static
+    {
+        $this->orders[$branch] = $preBranches;
+        return $this;
+    }
+
+    public function enableConcurrent()
+    {
+        $this->concurrent = true;
+    }
+
+    public function submit()
+    {
+        $this->addConcurrentContext();
+        return $this->api->submit($this->orders);
+    }
+
+    public function addConcurrentContext()
+    {
+        if ($this->concurrent) {
+            TransContext::setCustomData(json_encode([
+                'concurrent' => $this->concurrent,
+                'orders' => $this->orders,
+            ]));
+        }
+    }
+    
+}
