bf943d54d9917761bfc1c1914a0f68c73da097d2
pandaLIU <563883861@qq.com>
2022-02-09 01:41:28 +0800

    Updated Barrier

--- src/Aspect/BarrierAspect.php
+++ src/Aspect/BarrierAspect.php
@@ -25,37 +25,9 @@ class BarrierAspect extends AbstractAspect
 
     public function process(ProceedingJoinPoint $proceedingJoinPoint)
     {
-        /** @var RequestInterface $request */
-        $request = Context::get(RequestInterface::class);
-
-        $inputs = $request->all();
-
-        $op = $inputs[0]['op'];
-        $gid = $inputs[0]['gid'];
-        $branchId = $inputs[0]['branch_id'];
-        $transType = $inputs[0]['trans_type'];
-        TransContext::setGid();
-        TransContext::setBranchId($branchId);
-        TransContext::setTransType($transType);
-        TransContext::setOp($op);
-
-        TransContext::setBarrierID(TransContext::getBarrierID() + 1);
-        $barrierID = TransContext::getBarrierID();
-        $bid = sprintf('%02d', $barrierID);
-
-        $originOP = $opMap[$op] ?? '';
-        DB::beginTransaction();
-        try {
-            \DtmClient\Barrier::insertBarrier($transType, $gid, $branchId, $originOP, $bid, $barrierID);
-            \DtmClient\Barrier::insertBarrier($transType, $gid, $branchId, $op, $bid, $barrierID);
-            $result = $proceedingJoinPoint->process();
-            DB::commit();
-            return $result;
-        } catch (\Throwable $throwable) {
-            DB::rollback();
-            throw $throwable;
-        }
-
+        return \DtmClient\Barrier::call(function () use ($proceedingJoinPoint) {
+            return $proceedingJoinPoint->process();
+        });
 
     }
 
