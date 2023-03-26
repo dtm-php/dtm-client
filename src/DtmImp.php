313f6ad0866d7471fb8464f0be817b1d5ce5b134
pandaLIU <563883861@qq.com>
2023-03-26 13:15:20 +0800

    Fix the MySql XA deadlock

--- src/DtmImp.php
+++ src/DtmImp.php
@@ -77,6 +77,10 @@ class DtmImp
             $sql = $this->DBSpecial->getXaSQL('end', $xaId);
             $this->dbTransaction->xaExec($sql);
             $sql = $this->DBSpecial->getXaSQL('prepare', $xaId);
+
+            // Fix the MySql XA deadlock
+            $this->dbTransaction->reconnect();
+
             $this->dbTransaction->xaExec($sql);
         } catch (PDOException $exception) {
             throw $exception;
